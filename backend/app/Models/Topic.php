321bbc641ecacffcac446c2412a99838f8320429
Okeke kenechukwu Mcdavid <keneokeke4@gmail.com>
2026-08-28 20:14:17 +0000

    topic model done

--- backend/app/Models/Topic.php
+++ backend/app/Models/Topic.php
@@ -1,10 +1,20 @@
 <?php
 
 namespace App\Models;
+use App\Models\user;
+use App\Models\Review;
 
 use Illuminate\Database\Eloquent\Model;
 
 class Topic extends Model
 {
-    //
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function reviews()
+    {
+        return $this->hasMany(Review::class);
+    }
 }
