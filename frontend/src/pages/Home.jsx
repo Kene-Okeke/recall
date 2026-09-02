bdcd960459302fa785ddfc89bfe453bf6479cf90
Okeke kenechukwu Mcdavid <keneokeke4@gmail.com>
2026-09-02 00:37:39 +0000

    full streak + queuee page ui done

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -24,23 +24,65 @@ function Home() {
           {" "}
           {/* this section is for the queue of tasks that are due today */}
           <div className="queueText">
-            <span>QUEUE (3) </span>
+            <span>QUEUE (4) </span>
             <div className="queueLine"></div>
           </div>
           {/* these are the tasks in the queue */}
-          <div className="queueItemContainer">
-            <div className="queueItemLeft">
-              <h2 className="queueCardTopic">Two Sum</h2>
-              <div className="categoryContflex">
-                <h2 className="queueCardCategory">ARRAYS</h2>
-                <h3 className="queueCardLastAttempt">. Last try 3d ago </h3>
+          <section className="Queue-List">
+            <div className="queueItemContainer">
+              <div className="queueItemLeft">
+                <h2 className="queueCardTopic">Two Sum</h2>
+                <div className="categoryContflex">
+                  <h2 className="queueCardCategory">ARRAYS</h2>
+                  <h3 className="queueCardLastAttempt">. Last try 3d ago </h3>
+                </div>
+              </div>
+
+              <div className="queueItemRight">
+                <h2 className="queueLastScore">41</h2>
               </div>
             </div>
 
-            <div className="queueItemRight">
-              <h2 className="queueLastScore">41</h2>
+            <div className="queueItemContainer">
+              <div className="queueItemLeft">
+                <h2 className="queueCardTopic">Binary Search</h2>
+                <div className="categoryContflex">
+                  <h2 className="queueCardCategory">BINARY SEARCH </h2>
+                  <h3 className="queueCardLastAttempt">. Last try 6d ago </h3>
+                </div>
+              </div>
+
+              <div className="queueItemRight">
+                <h2 className="queueLastScore">67</h2>
+              </div>
             </div>
-          </div>
+            <div className="queueItemContainer">
+              <div className="queueItemLeft">
+                <h2 className="queueCardTopic">Merge Intervals</h2>
+                <div className="categoryContflex">
+                  <h2 className="queueCardCategory">Intervals </h2>
+                  <h3 className="queueCardLastAttempt">. Last try 9d ago </h3>
+                </div>
+              </div>
+
+              <div className="queueItemRight">
+                <h2 className="queueLastScore">37</h2>
+              </div>
+            </div>
+            <div className="queueItemContainer">
+              <div className="queueItemLeft">
+                <h2 className="queueCardTopic">Binary Trees</h2>
+                <div className="categoryContflex">
+                  <h2 className="queueCardCategory">Trees</h2>
+                  <h3 className="queueCardLastAttempt">. Last try 6d ago </h3>
+                </div>
+              </div>
+
+              <div className="queueItemRight">
+                <h2 className="queueLastScore">28</h2>
+              </div>
+            </div>
+          </section>
         </section>
         {/* add new topic button */}
         <div className="addNewTopicButton">
